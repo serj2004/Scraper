c8b24ebc70dade3bd1404a9910e95fe30f6a80f2
Sergey <serg_2004@inbox.ru>
2020-08-02 12:42:35 +0700

    удалил лишние строки

--- main.py
+++ main.py
@@ -1,31 +1,6 @@
-# from selenium import webdriver
-# driver = webdriver.Chrome("C:/Users/Сергей/IdeaProjects/testselenium/drivers/chromedriver.exe")
-# driver.get("https://yandex.ru/")
-# from typing import TextIO
-# import csv
 import flask
-
-# app = flask.Flask(__name__)
-# @app.route('/')
-# def index():
-#     return "Hello world!"
-# app.run(port='8000')
-
 import re
 import this
-# my_list = list()
-# with open('C:\\Users\Сергей\Desktop\файлы 10мб\File_113.csv', "r",) as f:
-#     r = csv.reader(f, delimiter=",")
-#     for row in r:
-#         print(",".join(row))
-#w = csv.writer(f, delimiter=",")
-#w.writerow(["один","два", "три"])
-#w.writerow(["четыре", "пять", "шесть"])
-
-# f.write("Пще однаА строка.")
-# my_list.append(f.read())
-# print(my_list)
-
 
 
 
